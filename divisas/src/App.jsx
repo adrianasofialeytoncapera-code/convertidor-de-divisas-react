2e93ef2ccf70a516620d0c5b6222fc50b543458f
Sofia Leyton <adrianasofialeytoncaperra@gmail.com>
2026-03-03 10:17:10 -0500

    se realizaron unos cambios del archivo

--- divisas/src/App.jsx
+++ divisas/src/App.jsx
@@ -1,34 +1,56 @@
-import {useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 
-const App = () => { 
+const App = () => {
   const [divisas, setDivisas] = useState([])
+  const[seleccion, setSeleccion] = useState('')
+  const [divisaDefinitiva, setDivisaDefinitiva] = useState(null)
 
   useEffect(() => {
-    consultar()
-  console.log("al renderizar o cambiar el valor de las divisas")
+   consultar()
    console.log(divisas)
   }, [])
 
-  const consultar=async () => {
-  let url = "https://co.dolarapi.com/v1/cotizaciones"
-  const resultado = await fetch(url)
-  setDivisas(resultado.json())
+  const consultar = async () => {
+    let url = "https://co.dolarapi.com/v1/cotizaciones"
+     const resultado = await fetch(url)
+     const data = await resultado.json()
+     setDivisas(data)
+     return resultado.json()
   }
-  
+  const cambioDivisa = (idDivisa) => {
+    console.log("divisa seleccionada",idDivisa)
+    setSeleccion(idDivisa)
+    setDivisaDefinitiva(buscarDivisa(idDivisa))
+    console.log("divisa encontrada", divisaDefinitiva)
+  }
+
+  const buscarDivisa = (idDivisa) => {
+    let divisaEncontrada = divisas.find(objdivisa => 
+      objdivisa.moneda === idDivisa)
+      
+    return divisaEncontrada
+  }
+
   return (
-   <>
-   <div className="contenedor">
+    <>  <div className="contenedor">
         <h1>Convertir desde COP</h1>
         <input type="number" id="valor" placeholder="Cantidad en pesos (COP)"/>
-        
-        <select id="opcionesDivisas">
+      
+        <select id="opcionesDivisas"
+         onChange={(evento) => cambioDivisa(evento.target.value)}>
             <option value="">Cargando divisas...</option>
+        { divisas &&
+          divisas.map (divisa => {
+            return <option value={divisa.moneda}>{divisa.nombre}</option>
+          })
+        }
         </select>
 
-        <button >Convertir</button>
+        { <button >Convertir</button> }
         <p id="resultado"></p>
+        {divisaDefinitiva?.ultimoCierre}
     </div>
-   </>
+</>
   )
 }
 
